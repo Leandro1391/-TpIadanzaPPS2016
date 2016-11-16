34bc83979b983b486d57d0ea71075371adaa6473
Leandro1391 <leandrojulian1391@gmail.com>
2016-11-16 00:49:11 -0300

    Se agrgaron las carpetas PHP y la BD

---
+++ TpFinal2016/www/PHP/clases/producto.php
@@ -0,0 +1,198 @@
+<?php
+require_once"AccesoDatos.php";
+class producto
+{
+//--------------------------------------------------------------------------------//
+//--ATRIBUTOS
+	public $id;
+	public $nombre;
+	public $local;
+	public $localidad;
+	public $direccion;
+ 	public $precio;
+ 	public $codbar;
+ 	public $foto;
+
+//--------------------------------------------------------------------------------//
+
+//--------------------------------------------------------------------------------//
+//--GETTERS Y SETTERS
+  	public function GetId()
+	{
+		return $this->id;
+	}
+	public function GetNombre()
+	{
+		return $this->nombre;
+	}
+	public function GetLocal()
+	{
+		return $this->local;
+	}
+	public function GetLocalidad()
+	{
+		return $this->localidad;
+	}
+	public function GetDireccion()
+	{
+		return $this->direccion;
+	}
+	public function GetPrecio()
+	{
+		return $this->precio;
+	}
+	public function GetCodbar()
+	{
+		return $this->codbar;
+	}
+	public function GetFoto()
+	{
+		return $this->foto;
+	}
+	
+
+	public function SetId($valor)
+	{
+		$this->id = $valor;
+	}
+	public function SetNombre($valor)
+	{
+		$this->nombre = $valor;
+	}
+	public function SetLocal($valor)
+	{
+		$this->local = $valor;
+	}
+	public function SetLocalidad($valor)
+	{
+		$this->localidad = $valor;
+	}
+	public function SetDireccion($valor)
+	{
+		$this->direccion = $valor;
+	}
+	public function SetPrecio($valor)
+	{
+		$this->precio = $valor;
+	}
+	public function SetCodbar($valor)
+	{
+		$this->codbar = $valor;
+	}
+	public function SetFoto($valor)
+	{
+		$this->foto = $valor;
+	}
+	
+	
+//--------------------------------------------------------------------------------//
+//--CONSTRUCTOR
+	public function __construct($id=NULL)
+	{
+		if($id != NULL){
+			$obj = Producto::TraerUnLocal($id);
+
+			$this->id = $id;		
+			$this->nombre = $obj->nombre;
+			$this->local = $obj->local;
+			$this->localidad = $obj->localidad;
+			$this->direccion = $obj->direccion;
+			$this->precio = $obj->precio;
+			$this->codbar = $obj->codbar;
+			$this->foto = $obj->foto;
+		}
+	}
+
+//--------------------------------------------------------------------------------//
+//--TOSTRING	
+  	public function ToString()
+	{
+	  	return $this->nombre."-".$this->local."-".$this->localidad."-".$this->direccion."-".$this->precio."-".$this->codbar."-".$this->foto;
+	}
+//--------------------------------------------------------------------------------//
+
+//--------------------------------------------------------------------------------//
+//--METODO DE CLASE
+	public static function TraerUnProducto($idParametro) 
+	{	
+
+
+		$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+		$consulta =$objetoAccesoDato->RetornarConsulta("SELECT * FROM misproductos WHERE id =:id");
+		//$consulta =$objetoAccesoDato->RetornarConsulta("CALL TraerUnProducto(:id)");
+		$consulta->bindValue(':id', $idParametro, PDO::PARAM_INT);
+		$consulta->execute();
+		$productoBuscado= $consulta->fetchObject('productos');
+		return $informeBuscado;	
+					
+	}
+	
+	public static function TraerTodosLosProductos()
+	{
+		$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+		$consulta =$objetoAccesoDato->RetornarConsulta("SELECT * FROM misproductos");
+		//$consulta =$objetoAccesoDato->RetornarConsulta("CALL TraerTodasLasProductos() ");
+		$consulta->execute();			
+		$arrInformes= $consulta->fetchAll(PDO::FETCH_CLASS, "productos");	
+		return $arrInformes;
+	}
+
+	public static function TraerProductosPorPrecio()
+	{
+		$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+		$consulta =$objetoAccesoDato->RetornarConsulta("SELECT * FROM misproductos ORDER BY precio");
+		$consulta->execute();			
+		$arrPersonas= $consulta->fetchAll(PDO::FETCH_CLASS, "productos");	
+		return $arrPersonas;
+	}
+	
+	public static function BorrarProducto($idParametro)
+	{	
+		$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+		$consulta =$objetoAccesoDato->RetornarConsulta("DELETE FROM misproductos	WHERE id=:id");	
+		//$consulta =$objetoAccesoDato->RetornarConsulta("CALL BorrarProducto(:id)");	
+		$consulta->bindValue(':id',$idParametro, PDO::PARAM_INT);		
+		$consulta->execute();
+		return $consulta->rowCount();
+		
+	}
+	
+	public static function ModificarProducto($producto)
+	{
+			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+			$consulta =$objetoAccesoDato->RetornarConsulta("UPDATE producto set nombre=:nombre,local=:local,localidad=:localidad,direccion=:direccion,precio=:precio,codbar=:codbar,foto=:foto WHERE id=:id");
+			$consulta->bindValue(':id',$producto->id, PDO::PARAM_INT);
+			$consulta->bindValue(':nombre',$producto->nombre, PDO::PARAM_STR);
+			$consulta->bindValue(':local',$producto->local, PDO::PARAM_STR);
+			$consulta->bindValue(':localidad',$producto->localidad, PDO::PARAM_STR);
+			$consulta->bindValue(':direccion', $producto->direccion, PDO::PARAM_STR);
+			$consulta->bindValue(':precio', $producto->precio, PDO::PARAM_STR);
+			$consulta->bindValue(':codbar', $producto->codbar, PDO::PARAM_STR);
+			$consulta->bindValue(':foto', $producto->foto, PDO::PARAM_STR);
+			return $consulta->execute();
+	}
+
+//--------------------------------------------------------------------------------//
+
+//--------------------------------------------------------------------------------//
+
+	public static function InsertarProducto($producto)
+	{
+		$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+		$consulta =$objetoAccesoDato->RetornarConsulta("INSERT into producto (nombre,local,localidad,direccion,precio,codbar,foto)values(:nombre,:local,:localidad,:direccion,:precio,:codbar,:foto)");
+		//$consulta =$objetoAccesoDato->RetornarConsulta("CALL Insertarlocal (:nombre,:apellido,:dni,:foto)");
+		$consulta->bindValue(':nombre',$producto->nombre, PDO::PARAM_STR);
+		$consulta->bindValue(':local',$producto->local, PDO::PARAM_STR);
+		$consulta->bindValue(':localidad',$producto->localidad, PDO::PARAM_STR);				
+		$consulta->bindValue(':direccion', $producto->direccion, PDO::PARAM_STR);
+		$consulta->bindValue(':precio', $producto->precio, PDO::PARAM_STR);
+		$consulta->bindValue(':codbar', $producto->codbar, PDO::PARAM_STR);
+		$consulta->bindValue(':foto', $producto->foto, PDO::PARAM_STR);
+		$consulta->execute();		
+		return $objetoAccesoDato->RetornarUltimoIdInsertado();
+	
+				
+	}		
+
+
+}
---
+++ TpFinal2016/www/bd/pps2016.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-06-2016 a las 23:38:29
+-- Versión del servidor: 5.6.21
+-- Versión de PHP: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `segundoparcial2016`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `misproductos`
+--
+DROP TABLE IF EXISTS `misproductos`;
+CREATE TABLE IF NOT EXISTS `misproductos` (
+`id` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `local` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `localidad` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `direccion` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  `precio` decimal(7,2) COLLATE utf8_spanish2_ci NOT NULL,
+  `codbar` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `foto` varchar(50) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `misproductos`
+--
+
+INSERT INTO `misproductos` (`id`, `nombre`, `local`, `localidad` , `direccion`, `precio`, `codbar`, `foto`) VALUES
+(1, 'cuaderno Ledesma', 'Jumbo', 'Quilmes', 'Av Calchaquí 3950', 90.55, '7791762453090', ''),
+(2, 'pintura verde inglés','Sagitario', 'Quilmes', 'José Craviotto 3693', 110.88, '7792952016972', ''),
+(3, '', 'Quilmes', 'Av Calchaquí 3950', '', 'si', 'si', 'si', 'si', 100, '10/10/2016'),
+(4, 'cuaderno Ledesma Essential', 'Walmart','Avellaneda', 'Carlos Gardel 3900', 80.99, '7791762559099', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `misusuarios`
+--
+DROP TABLE IF EXISTS `misusuarios`;
+CREATE TABLE IF NOT EXISTS `misusuarios` (
+`id` int(11) NOT NULL,
+  `correo` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `clave` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `tipo` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `foto` varchar(50) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `misusuarios`
+--
+
+INSERT INTO `misusuarios` (`id`, `correo`, `nombre`, `clave`, `tipo`,`foto`) VALUES
+(1, 'user@user.com', 'roger', '123', 'usuario','333333.jpg'),
+(2, 'admin@admin.com', 'admin', '321', 'administrador', 'pordefecto.png'),
+(4, 'cliente@cliente.com', 'julia', '987', 'cliente',  '888888.jpg'),
+(5, 'fox@fox.com', 'michael', 'abcd', 'usuario','444444.jpg'),
+(6, 'jobs@jobs.com', 'steve', '987', 'cliente','111111.jpg'),
+(7, 'bulzara@bulzara.com', 'freddy', 'qwer', 'cliente', '999999.jpg'),
+(8, 'cruise@cruise.com', 'tom', '987', 'cliente', '777777.jpg');
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mislocales`
+--
+
+DROP TABLE IF EXISTS `mislocales`;
+CREATE TABLE IF NOT EXISTS `mislocales` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `localidad` varchar(25) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `gerente` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `mislocales`
+--
+
+INSERT INTO `mislocales` (`id`, `nombre`, `localidad`, `direccion`, `gerente`) VALUES
+(2, 'Garbarino','Avellaneda','Av Gral Güemes 897', 'Griselda Gonzalez'),
+(4, 'Hoyts', 'Temperley', 'Av Hipólito Yrigoyen 10699', 'Pablo Britos'),
+(12, 'Musimundo','Quilmes', 'Av Calchaquí 3950', 'Gastón Gaudio'),
+(13, 'McDonalds','Lomas de Zamora', 'Av Hipólito Yrigoyen 8230', 'Anderson Grey'),
+(18, 'Compumundo','Quilmes', 'Av Calchaquí 3950', 'Julieta Vanegas'),
+(19, 'McDonalds','Avellaneda', 'Av Bartolomé Mitre 633', 'Jesica Grey');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `misproductos`
+--
+ALTER TABLE `misproductos`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `misusuarios`
+--
+ALTER TABLE `misusuarios`
+ ADD PRIMARY KEY (`id`);
+
+ --
+-- Indices de la tabla `mislocales`
+--
+ALTER TABLE `mislocales`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `misproductos`
+--
+ALTER TABLE `misproductos`
+MODIFY `id` int(8) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `misusuarios`
+--
+ALTER TABLE `misusuarios`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `mislocales`
+--
+ALTER TABLE `mislocales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
